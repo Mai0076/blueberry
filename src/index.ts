713f2918ab3ac2bfa4ca08729d6b6cde5948ef71
Mai0076 <makuma222@gmail.com>
2024-12-02 21:53:15 +0900

    p38/BigIntリテラルについて

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,12 @@ const binary = 0b1010; //2進数リテラル
 const octal = 0o755; //8進数リテラル
 const hexadecimal = 0xff; //16進数リテラル
 //10 493 255 と表示
-console.log(binary, octal, hexadecimal);
 const big = 1e8; //10の8乗
 const small = 4e-5;
 //100000000 0.00004 と表示
-console.log(big, small);
+
+//2.3.4
+const bignum: bigint = (123n + 456n) * 2n;
+//1158nと表示
+const result = 5n / 2n; //BigIntは整数のみなので小数点以下は切り捨て
+//2n
